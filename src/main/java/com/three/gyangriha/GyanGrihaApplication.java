f672f52ac96e9211fd4f9d399bc998f9a76064c6
iavinash <iavinashmishra@gmail.com>
2024-12-16 18:45:43 +0530

    Added: DTOs and Entities

---
+++ src/main/java/com/three/gyangriha/GyanGrihaApplication.java
@@ -0,0 +1,13 @@
+package com.three.gyangriha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyanGrihaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GyanGrihaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/three/gyangriha/GyangrihaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.three.gyangriha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyangrihaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
